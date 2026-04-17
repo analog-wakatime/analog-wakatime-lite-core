319d3dc083b7fc6be1993c8b94a14735e509defe
pablaofficeal <pavlogorosko56@gmail.com>
2026-04-17 13:28:51 +0200

    fmt: format go files 2026-04-17 13:28

--- backend/main.go
+++ backend/main.go
@@ -24,8 +24,26 @@ func main() {
 	f, _ := os.OpenFile("logs/main_core.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	gin.DefaultWriter = io.MultiWriter(f)
 	app := gin.Default()
-	// gin.SetMode(gin.ReleaseMode)
-	testrouters(app)
+	v1Docs := app.Group("/api/v1/docs")
+	{
+		v1Docs.GET("", func(c *gin.Context) {
+			c.File("swagger/index.html")
+		})
+
+		v1Docs.GET("/", func(c *gin.Context) {
+			c.File("swagger/index.html")
+		})
+
+		v1Docs.GET("/openapi.yaml", func(c *gin.Context) {
+			c.File("api.yaml")
+		})
+	}
+
+	v1 := app.Group("/api/v1")
+	{
+		testrouters(v1)
+
+	}
 
 	app.Use(cors.New(config.CorsConfig()))
 
